eb058ced49b8991ace02bf3c4ca0b63eec9245b3
Jade-Neve Blows <118060796+JadeNeve@users.noreply.github.com>
2025-04-15 23:25:15 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    // You can replace this with a dynamic UUID from somewhere if needed
     router.push('/payin/');
   }, [router]);
 
